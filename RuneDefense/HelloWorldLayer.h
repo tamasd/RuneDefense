bc9e70849f95d6803d613e744d352721727522a6
Tamás Demeter-Haludka <yorirou@gmail.com>
2012-07-12 18:10:23 +0100

    Initial commit.

---
+++ RuneDefense/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  RuneDefense
+//
+//  Created by Tamas Demeter-Haludka on 7/12/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
